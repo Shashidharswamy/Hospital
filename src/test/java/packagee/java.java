8238f56887a5bbdf6a6de1845e354e4344d214dd
DELL <DELL@DESKTOP-U424R0D>
2022-08-24 17:13:08 +0530

    sprint8

--- src/test/java/packagee/java.java
+++ src/test/java/packagee/java.java
@@ -4,6 +4,6 @@ public class java {
 public static void main(String[] args) {
 	System.out.println("Hello ");
 	System.out.println("manju puss pusss");
-	System.out.println("ooooooooooooooooooo");
+	
 }
 }
