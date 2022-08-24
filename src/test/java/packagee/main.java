72daa1ad942b6d67705cca9659cc961d083f7ce1
HP <HP@DESKTOP-QA7IFKP>
2022-08-24 15:06:09 +0530

    updated

--- src/test/java/packagee/main.java
+++ src/test/java/packagee/main.java
@@ -3,5 +3,6 @@ package packagee;
 public class main {
 public main() {
 	System.out.println("Hello guys");
+	System.out.println("Hi guys");
 }
 }
---
+++ src/test/java/packagee/manja2.java
@@ -0,0 +1,7 @@
+package packagee;
+
+public class manja2 {
+public static void main(String[] args) {
+	System.out.println("Hello mams");
+}
+}
