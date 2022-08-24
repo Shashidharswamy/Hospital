54769314d184816a4d6b2f9cab34ce1309294cff
manju <manju@DESKTOP-C926226>
2022-08-24 15:53:53 +0530

    updated

--- src/test/java/packagee/java.java
+++ src/test/java/packagee/java.java
@@ -3,6 +3,7 @@ package packagee;
 public class java {
 public static void main(String[] args) {
 	System.out.println("Hello ");
-	System.out.println("Hello hi by by...");
+	System.out.println("manju puss pusss");
+	System.out.println("ooooooooooooooooooo");
 }
 }
